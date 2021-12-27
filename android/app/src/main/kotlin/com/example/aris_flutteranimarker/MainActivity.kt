adcdab5f54a90e7af187fc57e6891ff164c24ac7
ariscybertech <ariscybertech@gmail.com>
2021-12-28 01:15:43 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/aris_flutteranimarker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aris_flutteranimarker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_map_marker_animation_example/lib/main.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_map_marker_animation_example/animate_markers_through_polygon.dart';
+import 'listen_locations_updates.dart';
+
+void main() {
+  runApp(FlutterMapMarkerAnimationRealTimeExample());
+  //runApp(FlutterMapMarkerAnimationExample());
+}
